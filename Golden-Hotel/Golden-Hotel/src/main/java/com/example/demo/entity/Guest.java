b4ddc13bca3779255b3634c15e5b6f67c815986a
NguyenHuan02 <nqhuan2002@gmail.com>
2022-12-23 14:31:40 +0700

    update guest

--- Golden-Hotel/Golden-Hotel/src/main/java/com/example/demo/entity/Guest.java
+++ Golden-Hotel/Golden-Hotel/src/main/java/com/example/demo/entity/Guest.java
@@ -24,7 +24,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name="Guest")
-
+@Data
 public class Guest {
       @Id
       @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,91 +42,5 @@ public class Guest {
       
       @OneToOne(cascade = CascadeType.ALL)
       @JoinColumn(name="id")
-      private Invoice invoice;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public Date getDob() {
-		return dob;
-	}
-
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
-
-	public boolean isGender() {
-		return gender;
-	}
-
-	public void setGender(boolean gender) {
-		this.gender = gender;
-	}
-
-	public String getPhoneno() {
-		return phoneno;
-	}
-
-	public void setPhoneno(String phoneno) {
-		this.phoneno = phoneno;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPid() {
-		return pid;
-	}
-
-	public void setPid(String pid) {
-		this.pid = pid;
-	}
-
-	public List<Booking> getBooking() {
-		return booking;
-	}
-
-	public void setBooking(List<Booking> booking) {
-		this.booking = booking;
-	}
-
-	public Invoice getInvoice() {
-		return invoice;
-	}
-
-	public void setInvoice(Invoice invoice) {
-		this.invoice = invoice;
-	}
-
-	
-
-	
-      
-      
+      private Invoice invoice;      
 }
